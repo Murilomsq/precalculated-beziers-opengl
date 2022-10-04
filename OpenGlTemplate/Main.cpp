2355f93bf3a33551ab9c75881ebca22d56bc0804
Murilo <murilomsqc@gmail.com>
2022-10-04 03:01:59 -0300

    Smooth Spotlight

--- OpenGlTemplate/Main.cpp
+++ OpenGlTemplate/Main.cpp
@@ -228,6 +228,7 @@ int main()
         lightingShader.setVec3("light.position", camera.Position);
         lightingShader.setVec3("light.direction", camera.Front);
         lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
         lightingShader.setVec3("viewPos", camera.Position);
 
 
